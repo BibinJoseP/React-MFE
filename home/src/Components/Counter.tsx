a878f97c273444bc5c92618f41ff422323f4d430
BibinJoseP <99177729+BibinJoseP@users.noreply.github.com>
2022-10-13 06:11:45 +0530

    updated

--- home/src/Components/Counter.tsx
+++ home/src/Components/Counter.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import './Counter.css';
 export const Counter = () => {
     const [count, setCount] = useState(0);
     const onIncrement = () => setCount(count + 1);
     const onDecrement = () => setCount(count - 1);
     return (
-      <div>
+      <div className="counter">
         <h3>shared App</h3>
         <p>Current count: <strong>{count}</strong></p>
         <button onClick={onIncrement}>+</button>
